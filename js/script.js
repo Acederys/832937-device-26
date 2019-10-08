7de9880113ebeac388f43ec7179614f810671a85
Alexandra <acederys@gmail.com>
2019-10-08 20:34:09 +0300

    добавлена проверка формы

--- js/script.js
+++ js/script.js
@@ -1,26 +1,61 @@
 var linkPopap = document.querySelector('.link-popap');
-var popapLogin = document.querySelector('.modal');
-var closeLogin = popapLogin.querySelector('.close-popap');
-var popapMap = document.querySelector('.map-modal');
+var popupLogin = document.querySelector('.modal');
+var closeLogin = popupLogin.querySelector('.close-popap');
+var popupMap = document.querySelector('.map-modal');
 var linkMap = document.querySelector('.link-map');
-var closeMap = popapMap.querySelector('.close-map');
+var closeMap = popupMap.querySelector('.close-map');
+
+var form = popupLogin.querySelector('.modal-form');
+var inputName = popupLogin.querySelector('.input-name');
+var email = popupLogin.querySelector('.input-email');
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("inputName");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 linkPopap.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    popapLogin.classList.add('modal-on');
+  evt.preventDefault();
+  popupLogin.classList.add('modal-on');
+  if (storage) {
+    inputName.value = storage;
+    email.focus();
+    email.classList.add('modal-input-required');
+  } else {
+    inputName.focus();
+    inputName.classList.add('modal-input-required');
+  }
 });
 
 closeLogin.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    popapLogin.classList.remove('modal-on');
+  evt.preventDefault();
+  popupLogin.classList.remove('modal-on');
 });
 
+
 linkMap.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    popapMap.classList.add('map-modal-on');
+  evt.preventDefault();
+  popupMap.classList.add('map-modal-on');
 });
 
 closeMap.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popupMap.classList.remove('map-modal-on');
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!inputName.value || !email.value) {
     evt.preventDefault();
-    popapMap.classList.remove('map-modal-on');
+    popupLogin.classList.remove("modal-error");
+    popupLogin.offsetWidth = popupLogin.offsetWidth;
+    popupLogin.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("inputName", inputName.value);
+    }
+  }
 });
